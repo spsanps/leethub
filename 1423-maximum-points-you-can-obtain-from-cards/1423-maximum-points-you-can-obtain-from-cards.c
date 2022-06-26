343aeef67e1073f799cea3c8872ccd8d4018f414
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-06-26 12:57:56 +0530

    Time: 77 ms (38.46%), Space: 11.9 MB (7.69%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.c
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.c
@@ -3,36 +3,24 @@
 int maxScore(int* cP, int cPS, int k){
     
     
-
-    int lsum, rsum;
-    lsum = 0; rsum = 0;
-    
-    
-    int c = 0;
+  
+    int c;
     
     int* l_csum = (int*)malloc(sizeof(int)*(k+1));
-    int* r_csum = (int*)malloc(sizeof(int)*(k+1));
     
     l_csum[0] = 0;
-    r_csum[0] = 0;
     
-    while (c < k) {
-        lsum += cP[c];
-        rsum += cP[cPS - 1 - c];
-        l_csum[c + 1] = lsum;
-        r_csum[c + 1] = rsum;
-        c++;
-    }
+    for(c = 0; c < k; c++) l_csum[c + 1] = cP[c] + l_csum[c];
     
-    int sum = 0;
+    int r_csum = 0;
+    int sum = l_csum[k];
     int asum;
-    c = 0;
-    while (c <= k) {
-        asum = l_csum[c] + r_csum[k - c];
-        sum = asum > sum ? asum : sum;
-        c++;
+    
+    for(c = 0; c < k; c++) {
+        r_csum += cP[cPS - c - 1];
+        asum = l_csum[k - c - 1] + r_csum;
+        if (asum > sum) sum = asum;        
     }
-
 
     return sum;
     
