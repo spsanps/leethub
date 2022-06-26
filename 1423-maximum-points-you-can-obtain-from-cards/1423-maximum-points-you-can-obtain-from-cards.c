bf95d350d176241cf2ebdc99af1269546aad6d56
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-06-26 13:17:50 +0530

    Time: 67 ms (76.92%), Space: 11.4 MB (7.69%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.c
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.c
@@ -4,25 +4,18 @@ int maxScore(int* cP, int cPS, int k){
     
     
   
-    int c;
-    
-    int* l_csum = (int*)malloc(sizeof(int)*(k+1));
-    
-    l_csum[0] = 0;
-    
-    for(c = 0; c < k; c++) l_csum[c + 1] = cP[c] + l_csum[c];
-    
-    int r_csum = 0;
-    int sum = l_csum[k];
-    int asum;
-    
-    for(c = 0; c < k; c++) {
-        r_csum += cP[cPS - c - 1];
-        asum = l_csum[k - c - 1] + r_csum;
-        if (asum > sum) sum = asum;        
+    int i;
+    int lsum = 0;
+    for (i = 0; i < k; i++) {
+        lsum += cP[i];
     }
-
-    return sum;
+    int max = lsum;
+    for (i = 0; i < k; i++) {
+        lsum += cP[cPS - i - 1] - cP[k - i - 1];
+        if (lsum > max) max = lsum;
+    }
+    
+    return max;
     
 
 }
