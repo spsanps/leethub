61868ab7bf4d8a1d2fb39098f9a661d8aadf2b9a
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-06-28 16:24:06 +0530

    Time: 322 ms (58.18%), Space: 14.4 MB (74.55%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -10,29 +10,17 @@ class Solution(object):
             l[ord(c) - ord('a')] += 1
             
         l = [i for i in l if i != 0]
-        l.sort()
+        l.sort(reverse = True)
        
         c = 0
         for i in xrange(1, len(l)):
-            k = 0
-            while l[i - 1 - k] == l[i] - k and l[i] - k > 0:
-                k += 1
-                #print(l, l[i], k, l[i - 1 - k], l[i]- k, i -k)
-            if k > 0:
+            if l[i - 1] <= l[i]:
+                k = l[i] - l[i - 1]
+                if l[i - 1] != 0: k += 1
+                l[i] -= k
                 c += k
                 
                 
-                l.insert(i - k, l[i] - k)
-                
-                l.pop(i)
-                #print(l, len(l), i - k, l[i] - k)
-        
-                
-                
-       
-            
-            
-        #print(l)
         return c
             
             
