09aa8c265191da30a90e6f0599ecc41018388b31
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-06-28 16:25:09 +0530

    Time: 323 ms (57.73%), Space: 14.3 MB (74.55%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -15,8 +15,7 @@ class Solution(object):
         c = 0
         for i in xrange(1, len(l)):
             if l[i - 1] <= l[i]:
-                k = l[i] - l[i - 1]
-                if l[i - 1] != 0: k += 1
+                k = l[i] - l[i - 1] + (l[i - 1] != 0)
                 l[i] -= k
                 c += k
                 
