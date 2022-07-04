50a599587a151c6963a49a81cf50e7c706cc571c
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-07-04 14:48:18 +0530

    Time: 373 ms (5.18%), Space: 17.9 MB (5.18%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,59 @@
+class Solution(object):
+    def candy(self, r):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        
+        k = list(r)
+        
+        for i in xrange(len(r)):
+            if ((i == 0 or r[i - 1] == r[i]) and (i == len(r) - 1 or r[i] == r[i + 1])):
+                r[i] = 0
+        
+        #r = list(k)
+        #print(r)
+               
+        r_is = sorted(zip(r, range(len(r))), key = lambda x : x[0])
+            
+    
+        for v, r_i in r_is:
+            b = r[r_i]
+           
+            #print(b)
+            if (r_i == 0):
+                m = 0
+            else:
+                a = r[r_i - 1]
+                if (a >= b):
+                    m = 0
+                else:
+                    if (k[r_i - 1] == k[r_i]):
+                        m = 0
+                    else:
+                        m = a + 1
+                
+            if (r_i == len(r) - 1):
+                n = 0
+            else:
+                c = r[r_i + 1]
+                if (c >= b):
+                    n = 0
+                else:
+                    if (k[r_i + 1] == k[r_i]):
+                        n = 0
+                    else:
+                        n = c + 1
+
+            #print(r)
+            
+            #print(m, n)
+            
+            r[r_i] = max(m, n)
+            
+            #print(r)
+            
+        return len(r) + sum(r)
+            
+            
+        
