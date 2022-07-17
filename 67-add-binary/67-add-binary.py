2a058b7882af0fe644c735a5ce6b842067709624
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-07-17 12:23:16 +0530

    Time: 22 ms (87.34%), Space: 13.5 MB (74.68%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -5,4 +5,48 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        return bin( int(a, 2) + int(b, 2) )[2:]
+        c = 0
+        a = a[::-1]
+        b = b[::-1]
+        res = ''
+        for i in range(max(len(a), len(b))):
+            if i < len(a):
+                if a[i] == '1':
+                    ai = 1
+                else:
+                    ai = 0
+            else:
+                ai = 0
+            
+            if i < len(b):
+                if b[i] == '1':
+                    bi = 1
+                else:
+                    bi = 0
+            else:
+                bi = 0
+                
+            r = ai + bi + c
+            
+            if r == 3:
+                res += '1'
+                c = 1
+            elif r == 2:
+                res += '0'
+                c = 1
+            elif r == 1:
+                c = 0
+                res += '1'
+            else:
+                c = 0
+                res += '0'
+            
+            
+        if c == 1: res += '1'
+            
+        return res[::-1]
+            
+            
+            
+            
+            
