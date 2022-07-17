2b130307d1d9ddd925cc02a914edbc61290b2f94
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-07-18 00:21:29 +0530

    Time: 149 ms (26.34%), Space: 17.6 MB (6.85%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -3,30 +3,36 @@ class Solution:
         
         
         if len(Is) == 0: return [nI]
-        def bs(A, l, r):
-            mid = 0
-            while True:
-                prev_mid = mid
-                mid = (l + r)//2
-                if mid == prev_mid: return mid
-                #print(l, r, mid)
-                if Is[mid][0] == A:
-                    return mid
-                elif Is[mid][0] > A:
-                    r = mid + 1
-                else:
-                    l = mid - 1
-                
-            return mid
+        
+        i = 0
+        l = 0
+        r = len(Is)
         
         na, nb = nI
         
-        i = bs(na, 0, len(Is) - 1)
+        while True:
+            prev_mid = i
+            i = (l + r)//2
+            if i == prev_mid: 
+                break
+                #print(l, r, mid)
+            elif Is[i][0] > na:
+                r = i + 1
+            elif Is[i][0] < na:
+                l = i - 1
+            else:
+                break
+                
+            
+        
+        
+        #i = mid
         
         
         while i < len(Is) and Is[i][0] < na :
             i += 1
-        while i > 0 and Is[i - 1][0] > na:
+        
+        if i > 0 and Is[i - 1][0] > na:
             i -= 1
         #print(i)
         
@@ -34,6 +40,8 @@ class Solution:
             Is.append(nI)
         else:
             Is.insert(i, nI)
+            
+        print(Is)
         
         if i > 0 and Is[i - 1][1] >= na:
             Is[i][0] = Is[i - 1][0]
