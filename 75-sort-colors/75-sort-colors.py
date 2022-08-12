0850a1c8d46519e94de82f6ddb0e6db0320cb767
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-08-12 11:18:19 +0530

    Time: 31 ms (50.11%), Space: 13.2 MB (88.54%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,19 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        from collections import Counter
+        zero, one = 0, 0
         
-        c = Counter(nums)
-        
-        for i in range(c[0]):
-            nums[i] = 0
-            
-        for i in range(c[0], c[0] + c[1]):
+        for n in nums:
+            if n == 0:
+                nums[zero] = 0
+                zero += 1
+            elif n == 1:
+                one += 1
+                
+        for i in range(zero, zero + one):
             nums[i] = 1
-            
-            
-        for i in range(c[0] + c[1], len(nums)):
+        
+        for i in range(zero + one, len(nums)):
             nums[i] = 2
+        
+                
         
         
         
