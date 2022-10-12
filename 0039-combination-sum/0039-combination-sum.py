831648d0009b1f9804421c6467e1fef408388d21
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-10-12 21:26:05 +0530

    Time: 127 ms (59.84%), Space: 13.8 MB (97.15%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -5,14 +5,8 @@ class Solution:
         
         def get_combs(c_i, T):
             sols =[]
-            #print('T:', T)
-            
-            #if T < 0: return []
-            #if T == 0: return [[]]
-            
             for i in range(c_i, len(Cs)):
                 c = Cs[i]
-                #print(c)
                 if c < T:
                     s = get_combs(i, T - c)          
                     s = [list(a + [c]) for a in s]
