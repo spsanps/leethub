477862f7848fb050648062d503c313874aef496b
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-10-12 21:02:50 +0530

    Time: 106 ms (74.21%), Space: 13.9 MB (97.15%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum(self, Cs: List[int], T: int) -> List[List[int]]:
+
+        Cs.sort()
+        
+        def get_combs(c_i, T):
+            sols =[]
+            #print('T:', T)
+            
+            if T < 0: return []
+            if T == 0: return [[]]
+            
+            for i in range(c_i, len(Cs)):
+                c = Cs[i]
+                #print(c)
+                if c < T:
+                    s = get_combs(i, T - c)          
+                    s = [list(a + [c]) for a in s]
+                    sols.extend(s)
+                elif c > T:
+                    break
+                else:    
+                    sols.append([c])
+                    break
+            return sols
+        
+        return get_combs(0, T)
+            
