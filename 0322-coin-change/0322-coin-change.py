9e7d21bb5d3ab0da61e1d45b4c4452a36f6c8a5b
Sanjayan Pradeep Kumar Sreekala <sanjayanps@gmail.com>
2022-10-13 20:36:44 +0530

    Time: 6245 ms (5.00%), Space: 18.1 MB (22.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,7 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        
+        """        
         dp_dict = {}
         
         def dp(c_i, amount):
@@ -23,7 +23,45 @@ class Solution:
             dp_dict[(c_i, amount)] = ans 
             
             return ans
+        
                 
         ans = dp(0, amount)
         return ans if ans < math.inf else -1
+        """
+        
+       
+        tabulation = [list([0]*(amount + 1)) for i in range(len(coins) + 1)]
+        """
+        def print_tab():
+            for i in range(len(coins) + 1):
+                for j in range(amount + 1):
+                    print(tabulation[i][j], end = ' ')
+                print()
+            print()
+        """
+        #print_tab()
+        
+        for j in range(1, amount + 1): # when amount is 0, it is okay
+            tabulation[0][j] = math.inf # no solutions when not picking any coins
+        
+        #print_tab()
+        
+        for i in range(len(coins)):
+            tabulation[i][0] = 0
+        
+        #print_tab()
+        
+        for i in range(1, len(coins) + 1):
+            for j in range(amount + 1):
+                c = coins[i - 1]
+                if c > j:
+                    tabulation[i][j] = tabulation[i - 1][j]
+                else:
+                    tabulation[i][j] = min(1 + tabulation[i][j - c], 0 + tabulation[i - 1][j])
                 
+                #print_tab()
+
+        
+        ans = tabulation[len(coins)][amount] 
+        
+        return ans if ans < math.inf else -1
